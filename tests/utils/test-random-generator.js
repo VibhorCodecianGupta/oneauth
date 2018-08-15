0ac1578651b872fa89405a6241ac8c39596267c2
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-08-15 19:50:33 +0530

    circle ci

--- tests/utils/test-random-generator.js
+++ tests/utils/test-random-generator.js
@@ -1,15 +1,10 @@
-const chai = require('chai')
-var assert = require('assert')
-var expect = require('chai').expect;
-var should = require('chai').should();
+const expect = require('chai').expect;
 const { genNdigitNum } = require('../../src/utils/generator')
 
 describe('Block', function(){
-  
   it('Should return integer', function(){
-    
     var random = genNdigitNum(4)
     expect(random).to.be.a('number')
   })
-  
+
 })
